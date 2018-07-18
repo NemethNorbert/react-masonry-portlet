2294a9e55c534dfe95ce9d9cd50645229345a691
NemethNorbert <norbert.nemeth@liferay.com>
2018-07-18 16:06:05 +0200

    GROW-532 Use articleId as key to iterate trough the articles instead of the array index

--- app/Gallery.js
+++ app/Gallery.js
@@ -10,9 +10,9 @@ const imagesLoadedOptions = { }
 
 class Gallery extends Component {
     render() {
-        const childElements = this.props.elements.map(function(element, index){
+        const childElements = this.props.elements.map(function(element){
            return (
-            <div key={index} className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            <div key={element.articleId} className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
               <div className="rwp-article">
                 <div className="rwp-profile">
                   <img src="https://images.unsplash.com/photo-1484186139897-d5fc6b908812?ixlib=rb-0.3.5&s=9358d797b2e1370884aa51b0ab94f706&auto=format&fit=crop&w=200&q=80%20500w" className="thumbnailPic" />
